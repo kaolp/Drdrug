1dc4b3a431d0992f63993262de192c8edd5e12ec
kaolp <kaolipin@gmail.com>
2018-07-25 22:17:30 -0400

    Add files via upload

---
+++ pharmacy_counting-master/src/pharmacy-counting.py
@@ -0,0 +1,22 @@
+import csv
+drugNameDict={}
+with open ('..\input\itcont.txt', 'r') as f:
+    reader=csv.DictReader(f)
+    for row in reader:
+        myDrugName=row['drug_name']
+        idDict={}
+        if(myDrugName in drugNameDict):
+            idDict=drugNameDict.get(myDrugName)
+            idDict[row['id']]=row['drug_cost']
+        else:
+            idDict[row['id']]=row['drug_cost']
+            drugNameDict[row['drug_name']]=idDict
+for drugName in drugNameDict:
+    idDict=drugNameDict.get(drugName)
+    totalCost=0
+    count=0    
+    for id in idDict:
+        count=count + 1
+        drugCost=idDict.get(id)
+        totalCost=totalCost + float(drugCost)
+    print (drugName + ',' + str (count) + ',' + str(totalCost))
